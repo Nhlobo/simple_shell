3d4fdea2f9ee02eab7cd16b5956441f843ad9aaf
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-24 22:52:33 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,9 +1,4 @@
 #include "shell.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
 
 /**
  * execute - Execute a command in the shell
@@ -12,30 +7,21 @@
  */
 int execute(char *cmd)
 {
-    pid_t pid;
-    int status;
+	if (fork() == 0)
+	{
+		char *args[2] = {cmd, NULL};
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+		if (execve(cmd, args, NULL) == -1)
+		{
+			perror("Error");
+			_exit(EXIT_FAILURE);
+		}
+		_exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		wait(NULL);
+	}
 
-    if (pid == 0)
-    {
-        char *args[2] = {cmd, NULL};
-        if (execve(cmd, args, NULL) == -1)
-        {
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-        exit(EXIT_SUCCESS);
-    }
-    else
-    {
-        waitpid(pid, &status, WUNTRACED);
-    }
-
-    return 0;
+	return 0; /* Corrected line */
 }

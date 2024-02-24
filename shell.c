4b200b3a76f8028b453c51997e6899f3bf6eef57
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-24 22:32:19 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,22 +1,12 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - Entry point for the shell
- * Return: Always 0
- */
 int main(void)
 {
-	char *cmd;
+    char *cmd = "ls";
+    print_prompt();
+    execute(cmd);
 
-	while (1)
-	{
-		print_prompt();
-		cmd = read_line();
-		if (!cmd)
-			break;
-		execute(cmd);
-		free(cmd);
-	}
-
-	return 0; /* Corrected line */
+    return 0;
 }

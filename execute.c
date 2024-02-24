de42d7bdbcd4cd406c739ed65022c57580265a1e
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-24 22:26:04 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,4 +1,9 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 /**
  * execute - Execute a command in the shell
@@ -7,21 +12,33 @@
  */
 int execute(char *cmd)
 {
-	if (fork() == 0)
-	{
-		char *args[2] = {cmd, NULL};
+    pid_t pid, wpid;
+    int status;
 
-		if (execve(cmd, args, NULL) == -1)
-		{
-			perror("Error");
-			_exit(EXIT_FAILURE);
-		}
-		_exit(EXIT_SUCCESS);
-	}
-	else
-	{
-		wait(NULL);
-	}
+    pid = fork();
+    if (pid == -1)
+    {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
 
-	return 0; /* Corrected line */
+    if (pid == 0)
+    {
+        char *args[2] = {cmd, NULL};
+        if (execve(cmd, args, NULL) == -1)
+        {
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        do
+        {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 0;
 }

5b887f019b6d09e299ef462dd43aa3ec181a5813
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-24 17:04:25 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * Main function for the simple shell.
+ *
+ * It initializes the shell and enters a loop to read and execute commands until the user exits.
+ * The `winning_team` function is called before and after executing each command.
+ *
+ * @return 0 on successful execution.
+ */
 int main(void) {
     char command[100];
     winning_team();

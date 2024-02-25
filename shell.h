817021f6c11863cecd686227cca8f28dc24aaeff
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-25 10:50:17 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -8,5 +8,9 @@ char *read_line(void);
 void execute_command(char *command);
 void handle_builtin_commands(char *command);
 char *search_path(char *command);
+void handle_special_characters(char *command);
+void implement_redirections(char *command);
+void implement_pipes(char *command);
+void implement_cursor_movement(char *command);
 
 #endif /* SHELL_H */

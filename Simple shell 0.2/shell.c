ca43e89e00322761620e7fa32504b9db690a6bd8
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-25 13:52:58 +0200

    Create shell.c

---
+++ Simple shell 0.2/shell.c
@@ -0,0 +1,30 @@
+/* Filename: shell.c */
+
+#include "shell_header.h"
+
+/**
+ * main - Entry point for the simple shell
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    char *command;
+
+    while (1)
+    {
+        /* Display prompt */
+        write(1, "$ ", 2);
+
+        /* Read user input */
+        command = custom_read_function();
+
+        /* Execute the command with arguments */
+        custom_execute_function(command);
+
+        /* Free allocated memory, handle cleanup, etc. */
+        free(command);
+    }
+
+    return (0);
+}

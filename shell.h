372fe65ac4fd64fc04a6f00946da433442bd0cdd
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-25 10:43:38 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,7 +1,11 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+#include <stdio.h>
+
 void print_prompt(void);
 char *read_line(void);
+void execute_command(char *command);
+void handle_builtin_commands(char *command);
 
 #endif /* SHELL_H */

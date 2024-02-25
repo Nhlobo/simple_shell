3bcdb85698ebb4056a61a5ee19939f3a3e25ccfd
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-25 11:51:15 +0200

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,3 +1,5 @@
+// execute.c
+
 #include "shell.h"
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,6 +7,11 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/**
+ * Executes the given command.
+ * @param cmd The command to be executed.
+ * @return Returns 0 on successful execution.
+ */
 int execute(char *cmd) {
     pid_t pid, wpid;
     int status;
@@ -16,17 +23,24 @@ int execute(char *cmd) {
     }
 
     if (pid == 0) {
+        // Prepare arguments for execvp
         char *args[2] = {cmd, NULL};
+        
+        // Execute the command
         if (execvp(cmd, args) == -1) {
             perror("execvp");
             _exit(EXIT_FAILURE);
         }
+
+        // This code is unreachable after execvp
         _exit(EXIT_SUCCESS);
     } else {
+        // Parent process
         do {
             wpid = waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
 
+        // Check if the command was terminated by a signal
         if (WIFSIGNALED(status)) {
             fprintf(stderr, "Command terminated by signal %d\n", WTERMSIG(status));
         }

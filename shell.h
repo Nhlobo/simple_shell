1ad21ea2eb853a2e40af91ea61c3b5aa2c14ff24
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-25 10:46:38 +0200

    Update shell.h

--- shell.h
+++ shell.h
@@ -7,5 +7,6 @@ void print_prompt(void);
 char *read_line(void);
 void execute_command(char *command);
 void handle_builtin_commands(char *command);
+char *search_path(char *command);
 
 #endif /* SHELL_H */

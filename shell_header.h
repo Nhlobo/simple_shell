bff9a820d6bf11f3697f41a3d2d910186e289808
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-26 15:13:59 +0200

    Create shell_header.h

---
+++ shell_header.h
@@ -0,0 +1,10 @@
+#ifndef SHELL_HEADER_H
+#define SHELL_HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char *custom_read_function(void);
+void custom_execute_function(char *command);
+
+#endif /* SHELL_HEADER_H */

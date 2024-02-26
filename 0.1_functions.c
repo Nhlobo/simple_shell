10c7222f1a9538301daa871c3c24f1a16f8b6ba7
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-02-26 15:32:20 +0200

    Update 0.1_functions.c

--- 0.1_functions.c
+++ 0.1_functions.c
@@ -1,40 +1,15 @@
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <unistd.h>
 
-/**
- * custom_execute_function - Function to execute custom commands
- * @command: Command to be executed
- */
 void custom_execute_function(char *command)
 {
-    pid_t child_pid;
-    int status;
+    char *args[10];
+    args[0] = command;
+    args[1] = NULL;
 
-    child_pid = fork();
-    if (child_pid == -1)
+    if (execve(command, args, NULL) == -1)
     {
         perror("Error");
         exit(EXIT_FAILURE);
-    }
-
-    if (child_pid == 0)
-    {
-        /* Child process */
-        if (execve(command, NULL, NULL) == -1)
-        {
-            perror("Error");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        /* Parent process */
-        waitpid(child_pid, &status, 0);
     }
 }

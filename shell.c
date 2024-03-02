41f0b4adfebdbdc4903111966c496169cdd98f2d
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 19:45:29 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -1,6 +1,42 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-#define BUFFER_SIZE 1024
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARGS 128
+
+int parse_input(char *input, char **args) {
+    int num_args = 0;
+    char *token = strtok(input, " ");
+    
+    while (token != NULL && num_args < MAX_ARGS) {
+        args[num_args] = token;
+        num_args++;
+        token = strtok(NULL, " ");
+    }
+    
+    args[num_args] = NULL;
+    
+    return num_args;
+}
+
+void execute_command(char **args) {
+    pid_t pid = fork();
+    
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("Error");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("Error");
+    } else {
+        wait(NULL);
+    }
+}
 
 void run_shell() {
     char input[MAX_INPUT_SIZE];
@@ -10,16 +46,22 @@ void run_shell() {
         printf("SimpleShell> ");
         fgets(input, sizeof(input), stdin);
         
-        input[strcspn(input, "\n")] = '\0'; // Remove newline character from input
+        input[strcspn(input, "\n")] = '\0'; 
         
-        int num_args = parse_input(input, args); // Parse input into arguments
+        int num_args = parse_input(input, args);
         
         if (num_args > 0) {
             if (strcmp(args[0], "exit") == 0) {
-                break; // Exit the shell
+                break;
             } else {
-                execute_command(args); // Execute command
+                execute_command(args);
             }
         }
     }
+}
+
+int main() {
+    run_shell();
+    
+    return 0;
 }

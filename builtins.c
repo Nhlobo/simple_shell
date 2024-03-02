4a65b21027f27a32927528143bc84d78d2d5b6df
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 17:15:44 +0200

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,10 +1,12 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 
 int execute_command(char **args) {
     pid_t pid, wpid;
     int status;
+    int file_desc;
 
     char *path = search_path(args[0]);
     if (!path) {
@@ -15,6 +17,26 @@ int execute_command(char **args) {
     pid = fork();
     if (pid == 0) {
         // Child process
+        if (args[1] && strcmp(args[1], "<") == 0 && args[2]) {
+            file_desc = open(args[2], O_RDONLY);
+            if (file_desc < 0) {
+                perror("shell");
+                exit(EXIT_FAILURE);
+            }
+            dup2(file_desc, STDIN_FILENO);
+            close(file_desc);
+        }
+
+        if (args[1] && strcmp(args[1], ">") == 0 && args[2]) {
+            file_desc = open(args[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (file_desc < 0) {
+                perror("shell");
+                exit(EXIT_FAILURE);
+            }
+            dup2(file_desc, STDOUT_FILENO);
+            close(file_desc);
+        }
+
         if (execv(path, args) == -1) {
             perror("shell");
         }

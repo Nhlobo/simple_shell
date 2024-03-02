2b4c23630cd9361337e612ce40e5d52dc6457b0e
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-03 00:01:48 +0200

    Update betty would be proud.c

--- betty would be proud.c
+++ betty would be proud.c
@@ -1,25 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/**
- * main - Simple Shell
- *
- * Return: Always 0
- */
-int main(void)
-{
+int main(void) {
     char *input = NULL;
     size_t len = 0;
 
-    while (1)
-    {
-        printf("$ ");
+    while (1) {
+        printf("SimpleShell$ ");
         if (getline(&input, &len, stdin) == -1)
             break;
 
         // Your logic for handling user input and executing commands goes here
 
-        printf("$ ");
+        printf("SimpleShell$ ");
     }
 
     free(input);

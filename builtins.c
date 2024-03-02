d46ec30f522bdbb53a3b94d07c9b0581d154ac7f
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 17:03:43 +0200

    Create builtins.c

---
+++ builtins.c
@@ -0,0 +1,16 @@
+#include "builtins.h"
+
+int is_builtin(char **args) {
+    // Check if args[0] is a built-in command
+    // Return 1 if built-in, 0 if not
+}
+
+int execute_builtin(char **args) {
+    // Execute the built-in command
+    // Return status
+}
+
+int execute_command(char **args) {
+    // Execute external command
+    // Return status
+}

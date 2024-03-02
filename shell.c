2bc96e25f0bed2ee338619395704104639e3a79f
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 19:19:19 +0200

    Update shell.c

--- shell.c
+++ shell.c
@@ -3,17 +3,23 @@
 #define BUFFER_SIZE 1024
 
 void run_shell() {
-    char input[BUFFER_SIZE];
-
+    char input[MAX_INPUT_SIZE];
+    char *args[MAX_ARGS];
+    
     while (1) {
-        printf("SimpleShell$ ");
-        fgets(input, BUFFER_SIZE, stdin);
-
-        // Remove newline character
-        input[strcspn(input, "\n")] = 0;
-
-        // Implement your shell logic here
-        // For now, let's just print the input
-        printf("You entered: %s\n", input);
+        printf("SimpleShell> ");
+        fgets(input, sizeof(input), stdin);
+        
+        input[strcspn(input, "\n")] = '\0'; // Remove newline character from input
+        
+        int num_args = parse_input(input, args); // Parse input into arguments
+        
+        if (num_args > 0) {
+            if (strcmp(args[0], "exit") == 0) {
+                break; // Exit the shell
+            } else {
+                execute_command(args); // Execute command
+            }
+        }
     }
 }

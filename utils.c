fff3653990cfd7ad753fb59773c8844c0813312c
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 17:04:32 +0200

    Create utils.c

---
+++ utils.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void display_prompt() {
+    printf("#cisfun$ ");
+}
+
+char **parse_input(char *input) {
+    char **args = // Parse input into arguments
+    return args;
+}

6a508b74d958d4ff783f5f7e9c0479996a4636b3
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 17:08:11 +0200

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,11 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-void display_prompt() {
-    printf("#cisfun$ ");
-}
+#include <string.h>
 
 char **parse_input(char *input) {
-    char **args = // Parse input into arguments
+    // Split input string into arguments based on spaces
+    int buffer_size = 64;
+    int position = 0;
+    char **args = malloc(buffer_size * sizeof(char *));
+    char *arg;
+
+    if (!args) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+
+    arg = strtok(input, " \t\n");
+    while (arg != NULL) {
+        args[position] = arg;
+        position++;
+
+        if (position >= buffer_size) {
+            buffer_size += 64;
+            args = realloc(args, buffer_size * sizeof(char *));
+            if (!args) {
+                fprintf(stderr, "Memory allocation failed\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        arg = strtok(NULL, " \t\n");
+    }
+    
+    args[position] = NULL; // Null-terminate the argument list
     return args;
 }

1ef2a1804c74d3a46be251cbafa4a5de465d9889
Nhlobo <137711037+Nhlobo@users.noreply.github.com>
2024-03-02 17:13:34 +0200

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -1,16 +1,34 @@
-#include "builtins.h"
-
-int is_builtin(char **args) {
-    // Check if args[0] is a built-in command
-    // Return 1 if built-in, 0 if not
-}
-
-int execute_builtin(char **args) {
-    // Execute the built-in command
-    // Return status
-}
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int execute_command(char **args) {
-    // Execute external command
-    // Return status
+    pid_t pid, wpid;
+    int status;
+
+    char *path = search_path(args[0]);
+    if (!path) {
+        fprintf(stderr, "%s: command not found\n", args[0]);
+        return 1;
+    }
+
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        if (execv(path, args) == -1) {
+            perror("shell");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        // Error forking
+        perror("shell");
+    } else {
+        // Parent process
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    free(path);
+    return 1;
 }
